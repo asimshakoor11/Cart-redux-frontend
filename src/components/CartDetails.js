68bfd8eec366161241f60a566ff39a6a544b27c3
Asim Shakoor <asimshakoor920830@gmail.com>
2024-05-16 00:13:34 +0500

    third

--- src/components/CartDetails.js
+++ src/components/CartDetails.js
@@ -76,7 +76,7 @@ const CartDetails = () => {
         const headers = {
             "Content-Type":"application/json"
         }
-        const response = await fetch("http://localhost:7000/api/create-checkout-session",{
+        const response = await fetch("https://cart-redux-backend.vercel.app/api/create-checkout-session",{
             method:"POST",
             headers:headers,
             body:JSON.stringify(body)
